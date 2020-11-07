2e0a131b0d0ba29207758d388eefc6c58257fad6
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-08 03:30:44 +0530

    Initial Commit

---
+++ Minimum Steps.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; ++i) {
+        int k,m,n;
+        cin>>k>>m>>n;
+
+    }
+
+}
