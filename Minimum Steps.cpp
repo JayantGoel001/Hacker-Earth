b3327c8fba6251368bef80ef3093b4bb2cfb13b1
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-08 04:42:06 +0530

    Initial Commit

--- Minimum Steps.cpp
+++ Minimum Steps.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <math.h>
+#define ll long long int
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
@@ -6,9 +8,22 @@ int main(){
     int t;
     cin>>t;
     for (int i = 0; i < t; ++i) {
-        int k,m,n;
+        ll k,m,n;
         cin>>k>>m>>n;
-
+        ll count = 0;
+        while (k<m){
+            if (m%n==0){
+                m/=n;
+                count++;
+            }else{
+                ll x = (n-(m%n));
+                m+= (x/2)*2+x%2;
+                count+=x/2+x%2;
+            }
+        }
+        if (k>m){
+            count+=(k-m)/2+(k-m)%2;
+        }
+        cout<<count<<"\n";
     }
-
 }
