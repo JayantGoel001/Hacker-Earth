8ba0f97cae6760f52ad657ef23a88bbc8ceae7b0
username <54479676+CormacKrum@users.noreply.github.com>
2021-01-18 13:14:56 +0530

    Initial Commit

--- Mogu Loves Numbers.cpp
+++ Mogu Loves Numbers.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
     cin>>t;
     int tot = 1000000;
     bool isPrime[tot+1];
+    int ar[tot+1];
     isPrime[1]= false;
     for (int i = 2; i <= tot; ++i) {
         isPrime[i] = true;
-    }
-    for (int i = 2; i*i <=tot ; ++i) {
-        for (int j = i*i; j <tot ; j+=i) {
-            isPrime[j]= false;
-        }
-    }
-    int ar[tot+1];
-    for (int i = 0; i <=tot; ++i) {
         ar[i]=0;
+    }
+    ar[1] = 0;
+    for (int i = 2; i*i <=tot ; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= tot; j += i) {
+                isPrime[j] = false;
+            }
+        }
     }
     for (int i = 2; i <=tot; ++i) {
         if (isPrime[i-1]){
@@ -29,8 +32,10 @@ int main(){
     for (int i = 0; i < t; ++i) {
         int L,R;
         cin>>L>>R;
-        int count = ar[max(L,R)] - ar[min(L,R)];
-        if(isPrime[max(L,R)] ) {
+        int maxi = max(L,R);
+        int mini = min(L,R);
+        int count = ar[maxi] - ar[mini];
+        if(isPrime[maxi] ) {
             count++;
         }
         cout << count << "\n";
